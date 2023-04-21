d1785849751e9b5b1934995e346954155d50e269
unknown <linardssils1@gmail.com>
2023-04-21 09:13:05 +0300

    Adding info field

---
+++ Assets/Scripts/TextField.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextField : MonoBehaviour {
+
+	public GameObject nameField;
+	public GameObject ageField;
+	public GameObject textField;
+
+	public void showText()
+	{ 
+		textField.GetComponent<Text>().text = nameField.GetComponent<InputField>().text + "\n" + ageField.GetComponent<InputField>().text;
+    }
+}
